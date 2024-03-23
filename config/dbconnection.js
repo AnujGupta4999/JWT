b40de7e644ab7278af49f494d6a52a025eeea52f
Anuj Gupta <89295349+AnujGupta4999@users.noreply.github.com>
2024-03-23 23:01:03 +0530

    Update dbconnection.js

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -1,20 +1,5 @@
-// const mongoose = require('mongoose');
-
-// async function connect2(req, res, next) {
-//     mongoose.connect(url).then(()=>{
-//         console.log('MongoDB Connected...');
-//     }).catch((err)=>{
-//         console.log('Error while creating MongoDB connection ',err);
-//     })
-// };
-
-// module.exports = connect2;
-
-
 const mongoose = require('mongoose');
-// const url = process.env.MONGO_URL;
 
-// const encodedPassword = encodeURIComponent();
 async function connect() {
     try {
         await mongoose.connect("mongodb://localhost:27017/practicedb", {
